a8e3bd709da9072abf4ba3250f431ef57a46f26c
GovinduChathumina <46335009+GovinduChathumina@users.noreply.github.com>
2021-02-03 22:47:47 +0530

    updated product page designs

--- src/components/section-components/product-category.js
+++ src/components/section-components/product-category.js
@@ -12,161 +12,138 @@ class ProductCategory extends Component {
 			  <div className="container">
 			    <div className="row">
 			      <div className="col-lg-8 order-lg-last go-top">
-			        <div className="all-item-section all-item-area-2">        
-			          <div className="row">
-			            {/* gallery item start here */}
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/7.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="/product-details">Web Platform</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/1.png"} alt="img" />
-			                  <span>Paul E. Hensley</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/8.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="/product-details">Case Study</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/2.png"} alt="img" />
-			                  <span>Clay J. Barfield</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/9.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="product-details">Project Collaboration</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/3.png"} alt="img" />
-			                  <span>Duane A. Oliva</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/10.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="/product-details">Voice Landing Page</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/4.png"} alt="img" />
-			                  <span>Cynthia V. Sylvia</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/11.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="product-details">Clean Dashboard</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/5.png"} alt="img" />
-			                  <span>Paul E. Hensley</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
-			            <div className="all-isotope-item col-lg-5 col-sm-6">
-			              <div className="thumb">
-			                <a className="gallery-fancybox" href="#">
-			                  <img src={publicUrl+"assets/img/item/12.png"} alt="image" />
-			                </a>
-			                <a className="btn btn-white" href="#">Live Preview</a>
-			              </div>
-			              <div className="item-details">
-			                <h4><Link to="/product-details">Website Redesign</Link></h4>
-			                <p>Photography Word Press Theme</p>
-			                <span className="ratting">
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star" />
-			                  <i className="fa fa-star star-o" />
-			                  <span>(12)</span>
-			                </span>
-			                <a href="#" className="author-details align-item-center">
-			                  <img src={publicUrl+"assets/img/author/6.png"} alt="img" />
-			                  <span>Frances R. Leming</span>
-			                </a>
-			                <span className="price bg-white float-right">$30.00</span>
-			              </div>
-			            </div>
+				  <div className="all-item-section go-top">        
+			      <div className="item-isotope row">
+			        <div className="item-sizer col-1" />
+			        {/* gallery item start here */}
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-1 cat-3">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/1.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
 			          </div>
 			        </div>
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-2">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/2.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
+			          </div>
+			        </div>
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-3 cat-2">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/3.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
+			          </div>
+			        </div>
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-3 cat-1">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/4.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
+			          </div>
+			        </div>
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-2 cat-1">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/5.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
+			          </div>
+			        </div>
+			        <div className="all-isotope-item col-lg-4 col-sm-6 cat-1 cat-2">
+			          <div className="thumb">
+			            <a className="gallery-fancybox" href="#">
+			              <img src={publicUrl+"assets/img/item/6.png"} alt="image" />
+			            </a>
+			            <a className="btn btn-white" href="#">Live Preview</a>
+			          </div>
+			          <div className="item-details">
+			            <span className="price">$30.00</span>
+			            <span className="ratting float-right">
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star" />
+			              <i className="fa fa-star star-o" />
+			              <span>(12)</span>
+			            </span>
+			            <h4><Link to="/product-details">Kiamu</Link></h4>
+			            <p>Photography Word Press Theme</p>
+			          </div>
+			        </div>
+			      </div>
+			    </div>
 			        <div className="pagination-wrap text-center mt-2">
 			          <ul className="pagination pagination-2">
 			            <li className="page-item"><a className="page-link" href="#"><i className="la la-angle-left" /></a></li>
@@ -257,7 +234,7 @@ class ProductCategory extends Component {
 			              <Link to="/blog-details">Investment</Link>
 			              <Link to="/blog-details">CSS</Link>
 			              <Link to="/blog-details">Planing</Link>
-			              <Link to="/blog-details">Creative</Link>
+			              <Link to="/blog-details">Creative</Link>`
 			            </div>   
 			          </div> */}
 			        </div>
