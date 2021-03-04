6795dc9ff6bcc5a377f14c29608b5f4d2a0c0838
GovinduChathumina <46335009+GovinduChathumina@users.noreply.github.com>
2021-03-04 07:40:45 +0530

    Updated base url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://167.71.237.60/templates",
+  baseURL: "http://167.71.237.60:8081/templates",
   headers: {
     "Content-type": "application/json"
   }
